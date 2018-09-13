1810302d791f95e423382cf3c6e9c83b38e3589e
Khathawut <khathawut@Khathawuts-MacBook-Pro.local>
2018-09-13 16:06:41 +0700

    can scraping bus

--- nakhon.py
+++ nakhon.py
@@ -18,15 +18,79 @@ def parse(source,destination,date):
 
             parser = html.fromstring(response.text)
             # json_data_xpath = parser.xpath('//*[@id="subtab1"]/table/tbody/tr[1]//text()')
-            json_data_xpath = parser.xpath('//*[@id="subtab1"]/table/tbody//text()')
+            data_xpath = parser.xpath('//*[@id="subtab1"]/table/tbody//text()')
 
             lists = []
-            for i in json_data_xpath:
-                a = str(i)
-                if((a[0] >= 'A' and a[0] <='Z') or (a[0] >= 'a' and a[0] <= 'b') or (a[0] >= '0' and a[0] <= '9')):
-                    print(a)
-            # with open('xpath.txt','w') as fw:
-            #  	fw.write(json_data_xpath)
+            raw_list = []
+
+            raw_list.append('41')
+
+            for i in data_xpath:
+                dataStr = str(i)
+                if((dataStr[0] >= 'A' and dataStr[0] <='Z') or
+                (dataStr[0] >= 'a' and dataStr[0] <= 'b') or
+                (dataStr[0] >= '0' and dataStr[0] <= '9')):
+                    # print(dataStr)
+                    raw_list.append(dataStr)
+
+            # print(raw_list)
+
+            name = ''
+            departureTime = ''
+            arrivalTime = ''
+            pickup = ''
+            dropoff = ''
+            price = ''
+            count = 0
+
+            for data in raw_list:
+                if(count != len(raw_list) - 1):
+                    if(len(data) == 2 and data[0] >= '0' and data[0] <= '9'):
+                        # print('name: ', raw_list[count+1])
+                        name = raw_list[count+1]
+                        departureTime = raw_list[count+2]
+                    if(data == 'Dropoff: '):
+                        dropoff = raw_list[count+1]
+
+                    if(data == 'Estimated Arrival :'):
+                        time = raw_list[count+1]
+                        # print(time)
+                        if(time[0] >= '0' and time[0] <= '9'):
+                            arrivalTime = time
+                        else:
+                            arrivalTime = ''
+                    if(data == 'Pickup:'):
+                        pickup = raw_list[count+1]
+
+                    if(data[0] == 'T' and data[1] == 'H' and data[2] == 'B'):
+                        dataSplit = data.split()
+                        price = dataSplit[1]
+                        bus_info = {
+                        'name': name,
+                        'departureTime': departureTime,
+                        'arrivalTime': arrivalTime,
+                        'pickup': pickup,
+                        'dropoff': dropoff,
+                        'price': price
+                        }
+                        print('======================================')
+                        print('name', name)
+                        print('departureTime ', departureTime)
+                        print('arrivalTime ', arrivalTime)
+                        print('pickup ', pickup)
+                        print('dropoff ', dropoff)
+                        print('price ', price)
+                        print('======================================\n')
+                        lists.append(bus_info)
+                count += 1
+
+            # bus_data = json.dumps(lists)
+
+            # json_bus_data = json.loads(bus_data)
+            # print(lists)
+
+            with open('Bus-%s-%s-flight-results.json'%(source,destination),'w') as fp:
+             	json.dump(lists,fp,indent = 4)
 
             # raw_json =json.loads(json_data_xpath[0] if json_data_xpath else '')
             #
@@ -55,7 +119,7 @@ if __name__=="__main__":
     # print(source)
 
 
-    print ("Fetching flight details")
+    print ("Fetching Bus details")
     scraped_data = parse(source,destination,date)
     # print ("Writing data to output file")
     # with open('%s-%s-flight-results.json'%(source,destination),'w') as fp:
